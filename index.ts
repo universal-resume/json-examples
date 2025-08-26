3fc06234bbe35f2a8ba2566c2bb4bdbc4af428dd
Alexis Besson <alexis@besson.tech>
2025-08-26 15:28:34 +0200

    fix: minimize pictures size

--- index.ts
+++ index.ts
@@ -2,17 +2,17 @@ export const examples = [
 	{
 		name: "Dwight Schrute",
 		location:
-			"https://unpkg.com/@universal-resume/json-examples@2.0.0/examples/dwight-schrute.json",
+			"https://unpkg.com/@universal-resume/json-examples@2.0.1/examples/dwight-schrute.json",
 	},
 	{
 		name: "Bill Palmer",
 		location:
-			"https://unpkg.com/@universal-resume/json-examples@2.0.0/examples/bill-palmer.json",
+			"https://unpkg.com/@universal-resume/json-examples@2.0.1/examples/bill-palmer.json",
 	},
 	{
 		name: "Jerry Seinfeld",
 		location:
-			"https://unpkg.com/@universal-resume/json-examples@2.0.0/examples/jerry-seinfeld.json",
+			"https://unpkg.com/@universal-resume/json-examples@2.0.1/examples/jerry-seinfeld.json",
 	},
 ];
 
