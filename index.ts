4360c16f57368c2f697acbc408bd4d20822e5e04
Alexis Besson <alexis@besson.tech>
2025-08-26 15:09:07 +0200

    feat: add jerry seinfeld + pictures

--- index.ts
+++ index.ts
@@ -1,16 +1,19 @@
 export const examples = [
-    {
-        name: "Dwight Schrute",
-        location: "./examples/dwight-schrute.json"
-    },
-    {
-        name: "Bill Palmer",
-        location: "./examples/bill-palmer.json"
-    },
-    {
-        name: "Harry Potter",
-        location: "./examples/harry-potter.json"
-    },
-]
+	{
+		name: "Dwight Schrute",
+		location:
+			"https://unpkg.com/@universal-resume/json-examples@2.0.0/examples/dwight-schrute.json",
+	},
+	{
+		name: "Bill Palmer",
+		location:
+			"https://unpkg.com/@universal-resume/json-examples@2.0.0/examples/bill-palmer.json",
+	},
+	{
+		name: "Jerry Seinfeld",
+		location:
+			"https://unpkg.com/@universal-resume/json-examples@2.0.0/examples/jerry-seinfeld.json",
+	},
+];
 
-export default examples
+export default examples;
